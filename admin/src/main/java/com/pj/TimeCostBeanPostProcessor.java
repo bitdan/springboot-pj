4723a76bba8e1ec564a0deb13c17eb231a9b2c49
linger <abiasnip@gmail.com>
2024-03-28 16:12:32 +0800

    1.启动时间统计-2

--- admin/src/main/java/com/pj/TimeCostBeanPostProcessor.java
+++ admin/src/main/java/com/pj/TimeCostBeanPostProcessor.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @version 1.0
@@ -21,7 +22,7 @@ public class TimeCostBeanPostProcessor implements BeanPostProcessor {
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        costMap.put(beanName, System.currentTimeMillis());
+        costMap.put(beanName, System.nanoTime());
         return bean;
     }
 
@@ -29,10 +30,12 @@ public class TimeCostBeanPostProcessor implements BeanPostProcessor {
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if (costMap.containsKey(beanName)) {
             Long start = costMap.get(beanName);
-            long cost = System.currentTimeMillis() - start;
+            long cost = System.nanoTime() - start;
             if (cost > 0) {
                 costMap.put(beanName, cost);
-                log.info("bean: " + beanName + "\ttime: " + cost);
+                // 将纳秒值转换为毫秒值
+                long costInMs = TimeUnit.NANOSECONDS.toMillis(cost);
+                log.info("bean: " + beanName + "\ttime: " + costInMs);
             }
         }
         return bean;
