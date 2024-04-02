fb0b925ae37d5c43e6b6ee34e231f36339fc1ae6
linger <abiasnip@gmail.com>
2024-04-02 16:46:56 +0800

    1.stream-rabbit集成-调整

---
+++ admin/src/main/java/com/pj/config/CustomizationBean.java
@@ -0,0 +1,25 @@
+package com.pj.config;
+
+import io.undertow.server.DefaultByteBufferPool;
+import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+/**
+ * @version 1.0
+ * @description CustomizationBean
+ * @date 2024/4/2 16:44:45
+ */
+@Component
+public class CustomizationBean implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
+
+    @Override
+    public void customize(UndertowServletWebServerFactory factory) {
+        factory.addDeploymentInfoCustomizers(deploymentInfo -> {
+            WebSocketDeploymentInfo webSocketDeploymentInfo = new WebSocketDeploymentInfo();
+            webSocketDeploymentInfo.setBuffers(new DefaultByteBufferPool(false, 1024));
+            deploymentInfo.addServletContextAttribute("io.undertow.websockets.jsr.WebSocketDeploymentInfo", webSocketDeploymentInfo);
+        });
+    }
+}
--- service/src/main/java/com/pj/system/controller/MessageController.java
+++ service/src/main/java/com/pj/system/controller/MessageController.java
@@ -7,22 +7,24 @@ package com.pj.system.controller;
  */
 
 
-import com.pj.system.mq.MessageProviderImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @EnableBinding(Source.class)
 @RequiredArgsConstructor
 public class MessageController {
 
+    private final StreamBridge streamBridge;
 
-    private final MessageProviderImpl messageProvider;
     @GetMapping("/api/send")
     public void send() {
-        messageProvider.sendMethod();
+        streamBridge.send("output", UUID.randomUUID().toString());
     }
 }
